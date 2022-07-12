4c3f43f1a605bc33d09caeef38158d1ed69029f1
Lochrian-1 <workbusmax@gmail.com>
2022-07-12 21:07:38 +0200

    this better work

--- _printf.c
+++ _printf.c
@@ -3,35 +3,6 @@
 #include <stdarg.h>
 
 /**
- * check_specifier - specifically checks character type
- * @c: character.
- *
- * Return: pointer
- */
-
-int (*check_specifier(const char *c))(va_list)
-{
-	print_f printf[] = {
-		{'c', printc},
-		{'s', print_string},
-		{'d', print_n},
-		{'i', print_n},
-		{NULL, NULL}
-	};
-
-	int k;
-
-	for (k = 0; printf[k].p != '\0'; k++)
-	{
-		if (*c == *(printf[k].p))
-		{
-			return (printf[k].func);
-		}
-	}
-	return (NULL);
-}
-
-/**
  * _printf - Function that produces output according to a format
  * @format: character string
  *
